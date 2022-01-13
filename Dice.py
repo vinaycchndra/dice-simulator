ec7a86f75dbc7bca698974d7f8fb46ca4358b677
vinaycchndra <85540958+vinaycchndra@users.noreply.github.com>
2022-01-13 23:11:11 +0530

    Update Dice.py

--- Dice.py
+++ Dice.py
@@ -1,4 +1,4 @@
-import random 
+import random                         # Program that generates random numbers from 1 to 6 so that it can work like a dice simulator
 def rolling_dice(minimum=1,maximum=6):
     while True:
         if input('Do you want to roll the dice ? type y/n: ').lower()=='n':
