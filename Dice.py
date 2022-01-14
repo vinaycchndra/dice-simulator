c0ba0d6ba1b52b01eae87ad0e5e09a2c410dc5ad
vinaycchndra <85540958+vinaycchndra@users.noreply.github.com>
2022-01-14 12:06:33 +0530

    biased die

--- Dice.py
+++ Dice.py
@@ -1,9 +1,43 @@
-import random                         # Program that generates random numbers from 1 to 6 so that it can work like a dice simulator
-def rolling_dice(minimum=1,maximum=6):
-    print(random.randint(minimum,maximum))
-    while True:
-        if input('Do you want to roll again the dice ? type y/n: ').lower()=='n':
-            break
+import random                                                      # Dice simulator
+class dice_roll:
+    
+    def __init__(self,bias='n',inclined=0):
+        self.bias = bias
+        self.inclined = inclined
+        
+    def calling_function(self):                                    # Function for calling unbiased or biased die
+        if self.bias.lower() == 'y':
+            self.rolling_dice_biased()
         else:
-            print(random.randint(minimum,maximum))
-rolling_dice()
+            self.rolling_dice_unbiased()
+    
+    def rolling_dice_biased(self,minimum=1,maximum=6):             # Biased die simulator function
+        print(random.randint(minimum,maximum))
+        while True:
+            if input('Do you want to roll again the dice ? type Y/N: ').lower()=='n':
+                return 
+            else:
+                a = random.randint(minimum,maximum)
+                b = random.randint(minimum,maximum)
+                if b==self.inclined:                                # Condition that increases the probability of the appearance of the number
+                    print(b)
+                else:
+                    print(a)
+                    
+    def rolling_dice_unbiased(self,minimum=1,maximum=6):            # Unbiased die simulator
+        print(random.randint(minimum,maximum))
+        while True:
+            if input('Do you want to roll again the dice ? type Y/N: ').lower()=='n':
+                return  
+            else:
+                a = random.randint(minimum,maximum)
+                print(a)
+                
+                
+biased = input('Hey! do you want baised die? Y/N : ')
+if biased.lower()=='y':
+    inclined = int(input('Which number you want to appear more from 1 to 6? :'))
+    roll = dice_roll(biased,inclined)
+else:
+    roll = dice_roll()
+roll.calling_function()                                               # Method to call the function
